2d8fab8efe91476696f90da8d79146d042adfc6d
Randy Schmidt <me@r38y.com>
2015-12-30 18:34:05 -0500

    Integration test

--- spec/hubtran_spec.rb
+++ spec/hubtran_spec.rb
@@ -5,11 +5,16 @@ describe Hubtran do
     before do
       Hubtran.configure do |config|
         config.api_key = "api-key"
+        config.endpoint = "https://hubtran.dev"
       end
     end
 
     it "sets api_key" do
       expect(Hubtran.config.api_key).to eq "api-key"
+    end
+
+    it "sets endpoint" do
+      expect(Hubtran.config.endpoint).to eq "https://hubtran.dev"
     end
   end
 end
