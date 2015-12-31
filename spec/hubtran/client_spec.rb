da5b8e3fcd57dde16cc9ceef30be2c3278d176d1
Randy Schmidt <me@r38y.com>
2015-12-31 13:23:23 -0500

    Test authorization

--- spec/hubtran/client_spec.rb
+++ spec/hubtran/client_spec.rb
@@ -5,15 +5,42 @@ module Hubtran
     before do
       Hubtran.configure do |config|
         config.api_key = "test"
-        config.endpoint = "https://api.hubtran.test"
+        config.endpoint = "https://api.hubtran.dev"
       end
     end
 
-    it "does not asplode" do
-      described_class.new
+    it "works" do
+      VCR.use_cassette "client_valid_request" do
+        Hubtran.configure do |config|
+          config.api_key = "test"
+          config.endpoint = "https://api.hubtran.dev"
+          config.verify_ssl = false
+        end
+
+        response = described_class.new.put("/tms/loads/load_id", {
+          load: {load_id: "load_id"}
+        })
+        expect(response.status).to eq 200
+        json = JSON.parse(response.body)
+        expect(json["load"]["id"]).to eq 964138
+      end
     end
 
-    xit "returns an error if the api key is wrong" do
+    it "returns an error if the token is wrong" do
+      VCR.use_cassette "client_wrong_token" do
+        Hubtran.configure do |config|
+          config.api_key = "wrong"
+          config.endpoint = "https://api.hubtran.dev"
+          config.verify_ssl = false
+        end
+
+        response = described_class.new.put("/tms/loads/load_id", {
+          load_id: "load_id"
+        })
+        expect(response.status).to eq 401
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq "Token was incorrect"
+      end
     end
   end
 end
